a7362da28bd40d40bfe4722b3f6cf4c3ce6a77b0
Julius Schmitt <juliusjedermann@gmail.com>
2023-11-08 16:20:16 +0100

    fixed spectral norm

--- models/networks.py
+++ models/networks.py
@@ -1401,9 +1401,11 @@ class NLayerDiscriminator(nn.Module):
         kw = 4
         padw = 1
         if (no_antialias):
-            sequence = [self.apply_sn(nn.Conv2d(input_nc, ndf, kernel_size=kw, stride=2, padding=padw), nn.LeakyReLU(0.2, True))]
+            sequence = [spectral_norm(nn.Conv2d(input_nc, ndf, kernel_size=kw, stride=2, padding=padw)),
+                        nn.LeakyReLU(0.2, True)]
         else:
-            sequence = [self.apply_sn(nn.Conv2d(input_nc, ndf, kernel_size=kw, stride=1, padding=padw), nn.LeakyReLU(0.2, True)),
+            sequence = [spectral_norm(nn.Conv2d(input_nc, ndf, kernel_size=kw, stride=1, padding=padw)),
+                        nn.LeakyReLU(0.2, True),
                         Downsample(ndf)]
         nf_mult = 1
         nf_mult_prev = 1
@@ -1412,13 +1414,13 @@ class NLayerDiscriminator(nn.Module):
             nf_mult = min(2 ** n, 8)
             if (no_antialias):
                 sequence += [
-                    self.apply_sn(nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=kw, stride=2, padding=padw, bias=use_bias)),
+                    spectral_norm(nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=kw, stride=2, padding=padw, bias=use_bias)),
                     norm_layer(ndf * nf_mult),
                     nn.LeakyReLU(0.2, True)
                 ]
             else:
                 sequence += [
-                    self.apply_sn(nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=kw, stride=1, padding=padw, bias=use_bias)),
+                    spectral_norm(nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=kw, stride=1, padding=padw, bias=use_bias)),
                     norm_layer(ndf * nf_mult),
                     nn.LeakyReLU(0.2, True),
                     Downsample(ndf * nf_mult)]
@@ -1426,21 +1428,19 @@ class NLayerDiscriminator(nn.Module):
         nf_mult_prev = nf_mult
         nf_mult = min(2 ** n_layers, 8)
         sequence += [
-            self.apply_sn(nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=kw, stride=1, padding=padw, bias=use_bias)),
+            spectral_norm(nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=kw, stride=1, padding=padw, bias=use_bias)),
             norm_layer(ndf * nf_mult),
             nn.LeakyReLU(0.2, True)
         ]
 
         sequence += [
-            self.apply_sn(nn.Conv2d(ndf * nf_mult, 1, kernel_size=kw, stride=1, padding=padw))]  # output 1 channel prediction map
+            spectral_norm(nn.Conv2d(ndf * nf_mult, 1, kernel_size=kw, stride=1, padding=padw))]  # output 1 channel prediction map
         self.model = nn.Sequential(*sequence)
 
     def forward(self, input):
         """Standard forward."""
         return self.model(input)
 
-    def apply_sn(self, layer):
-        spectral_norm(layer) if self.use_spectral_norm else layer
 
 
 class PixelDiscriminator(nn.Module):
--- train.sh
+++ train.sh
@@ -1,9 +1,8 @@
 python train.py \
-  --name resnet_atn_07_from_05_adain \
+  --name resnet_atn_09_start_spectral_norm \
   --CUT_mode CUT \
   --dataroot dataset \
   --netG resnet_adain \
-  --no_random_mask \
+  --netD basic_spectral_norm \
   --n_epochs 15 \
   --n_epochs_decay 0 \
-  --continue_train \
