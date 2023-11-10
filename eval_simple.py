47fbe821e05466db99f2eb1b4c517d582004f50e
voidless <juliusjedermann@gmail.com>
2023-11-10 12:09:40 +0100

    added fid

--- eval_simple.py
+++ eval_simple.py
@@ -1,17 +1,44 @@
+from pathlib import Path
+
 import torch
+from torchvision.datasets import ImageFolder
+from torchvision.transforms import transforms
 from torch.utils.data import DataLoader
 from piq import FID
 
-from data import create_dataset
-from options.test_options import TestOptions
 
-opt = TestOptions().parse()
-dataset = create_dataset(opt)
+######
+name = ""
+epoch = 5
+batch_size = 4
+######
 
-exit()
+# Define the transforms for preprocessing the images
+transform = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.ToTensor(),
+    # transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])  # Adjust with the appropriate normalization values
+])
 
-first_dl, second_dl = DataLoader(), DataLoader()
+# Paths to your directories
+prefix = Path('results')/name/f'test_(epoch)'
+fake_images_path = prefix / 'fake_B'
+real_images_path = prefix / 'real_B'
+
+# Load images using ImageFolder and create DataLoaders
+fake_dataset = ImageFolder(root=fake_images_path, transform=transform)
+real_dataset = ImageFolder(root=real_images_path, transform=transform)
+
+fake_dl = DataLoader(fake_dataset, batch_size=batch_size, shuffle=False)
+real_dl = DataLoader(real_dataset, batch_size=batch_size, shuffle=False)
+
+# Initialize FID metric
 fid_metric = FID()
-first_feats = fid_metric.compute_feats(first_dl)
-second_feats = fid_metric.compute_feats(second_dl)
-fid: torch.Tensor = fid_metric(first_feats, second_feats)
+
+# Compute features using the valid DataLoaders
+fake_feats = fid_metric.compute_feats(fake_dl)
+real_feats = fid_metric.compute_feats(real_dl)
+
+# Compute FID
+fid_score = fid_metric(fake_feats, real_feats)
+print(f"The FID score is: {fid_score.item()}")
